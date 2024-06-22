fdc72f32e8892570ba6c94b50707de4b15b8caa0
ejprietocal <prietoeider1@gmail.com>
2024-06-22 17:28:27 -0500

    añadiendo sobre nosotros

--- public/index.php
+++ public/index.php
@@ -1,8 +1,10 @@
 <?php
 require_once __DIR__ . '/../includes/app.php';
 
-use Controllers\MainpageController;
 use MVC\Router;
+use Controllers\ContactoController;
+use Controllers\MainpageController;
+use Controllers\SobreNosotrosController;
 
 
 $router = new Router();
@@ -10,6 +12,8 @@ $router = new Router();
 
 
 $router->get('/',[MainpageController::class,'index']);
+$router->get('/contacto',[ContactoController::class,'index']);
+$router->get('/sobre-nosotros',[SobreNosotrosController::class,'index']);
 
 //aqui van las rutas
 
